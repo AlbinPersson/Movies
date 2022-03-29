44ef8c7ba339953b5408a11ccd8f6fdf8d772b50
albin <albinp2009@live.se>
2022-03-29 09:56:46 +0200

    init commit

---
+++ routes/movies.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Funkar");
+});
+
+export default router;
