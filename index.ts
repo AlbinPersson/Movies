e28dfefc569b02f16df79fd2a6787c7c0cdd7936
albin <albinp2009@live.se>
2022-04-07 20:32:59 +0200

    connected to mongoDB

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import movies from "routes/movies";
+
 const app = express();
 
 //middelware
@@ -9,6 +11,11 @@ app.use(express.json());
 
 //routes
 app.use("/api/movies", movies);
+
+mongoose
+  .connect("mongodb://localhost/movie")
+  .then(() => console.log("connected to MongDB"))
+  .catch((error) => console.log("could not connect to mongDB", error));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
