90028c7a719156b233434fa5b288bac2de278793
albin <albinp2009@live.se>
2022-04-07 20:32:40 +0200

    added enum for GenreNames

--- client/src/types/Genre.ts
+++ client/src/types/Genre.ts
@@ -1,3 +1,13 @@
+export enum GenreName {
+  DRAMA = "Drama",
+  CRIME = "Crime",
+  ACTION = "Action",
+  DOCUMENTARY = "Dokumentary",
+  THRILLER = "Thriller",
+  HISTORY = "History",
+  WAR = "War",
+}
+
 export interface Genre {
   _id: string;
   name: string;
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,2 +1,4 @@
-export type { Movie } from "types/Movie";
 export type { Genre } from "types/Genre";
+export { GenreName } from "types/Genre";
+export type { Movie } from "types/Movie";
+export { Category } from "types/Movie";
